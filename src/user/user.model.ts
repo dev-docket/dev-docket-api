6a380725e6f95f231a434b4b8ab8d33d0a162f5a
Błażej Domagała <petitoff@outlook.com>
2023-10-02 21:04:52 +0200

    added user model

---
+++ src/user/user.model.ts
@@ -0,0 +1,33 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../db/database';
+
+class User extends Model {
+  public id!: number;
+  public email!: string;
+  public password!: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: new DataTypes.STRING(128),
+      allowNull: false,
+    },
+    password: {
+      type: new DataTypes.STRING(128),
+      allowNull: false,
+    },
+  },
+  {
+    tableName: 'users',
+    sequelize,
+    timestamps: false,
+  },
+);
+
+export default User;
