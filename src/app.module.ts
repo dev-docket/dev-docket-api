18adeea71359d464034d42bf51237155488cb21a
Błażej Domagała <petitoff@outlook.com>
2023-10-02 21:06:33 +0200

    added UserController and AuthController to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [JwtModule],
+  controllers: [AppController, UserController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
