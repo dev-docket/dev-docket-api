de73e683583c133bfebc2cfbfd2d43f393e38280
Błażej Domagała <petitoff@outlook.com>
2023-10-03 20:12:06 +0200

    team model moved to teams folder

---
+++ src/teams/team.model.ts
@@ -0,0 +1,31 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../db/database';
+
+export class Team extends Model {
+  id: number;
+  name: string;
+  projectId: number;
+}
+
+Team.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    projectId: {
+      type: DataTypes.INTEGER,
+      field: 'project_id',
+    },
+  },
+  {
+    sequelize,
+    modelName: 'team',
+    tableName: 'teams',
+    timestamps: false,
+  },
+);
