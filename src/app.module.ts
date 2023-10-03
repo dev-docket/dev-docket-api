7cee384db4de7d3118206ac8eddf1ce33b83777e
Błażej Domagała <petitoff@outlook.com>
2023-10-03 12:04:50 +0200

    added new controllers with services

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,21 @@ import { UserController } from './user/user.controller';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { JwtModule } from './jwt/jwt.module';
+import { ProjectsController } from './projects/projects.controller';
+import { ProjectsModule } from './projects/projects.module';
+import { ProjectMembersController } from './project-members/project-members.controller';
+import { ProjectMembersService } from './project-members/project-members.service';
+import { ProjectsService } from './projects/projects.service';
 
 @Module({
-  imports: [JwtModule],
-  controllers: [AppController, UserController, AuthController],
-  providers: [AppService, AuthService],
+  imports: [JwtModule, ProjectsModule],
+  controllers: [
+    AppController,
+    UserController,
+    AuthController,
+    ProjectsController,
+    ProjectMembersController,
+  ],
+  providers: [AppService, AuthService, ProjectMembersService, ProjectsService],
 })
 export class AppModule {}
