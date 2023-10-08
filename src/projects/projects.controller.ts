afc15003bec3c4896837a98d6db5d5f1a9ab78b7
Błażej Domagała <petitoff@outlook.com>
2023-10-09 00:04:09 +0200

    api/v1 and getProject by slug

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -12,13 +12,24 @@ import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { ProjectMembersService } from 'src/project-members/project-members.service';
 
-@Controller('projects')
+@Controller('api/v1/projects')
 @ApiTags('Projects')
 export class ProjectsController {
   constructor(
     private readonly projectsService: ProjectsService,
     private readonly projectMembersService: ProjectMembersService,
   ) {}
+
+  @Get(':projectSlug')
+  @ApiOkResponse({
+    description: 'The project has been successfully returned.',
+    type: Project,
+  })
+  async getProject(
+    @Param('projectSlug') projectSlug: string,
+  ): Promise<Project> {
+    return await this.projectsService.getProject(projectSlug);
+  }
 
   @Get('users/:userId')
   @ApiOkResponse({
