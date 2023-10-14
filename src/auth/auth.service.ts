9cffead6dcb9315a94335f674645e1aa2aa70077
Błażej Domagała <petitoff@outlook.com>
2023-10-14 15:31:26 +0200

    feat: return UNAUTHORIZED status if bad login or password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { LoginUserDto } from './dto/login-user.dto';
 import * as bcrypt from 'bcrypt';
@@ -6,6 +6,8 @@ import User from 'src/user/user.model';
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
+
   constructor(private jwtService: JwtService) {}
 
   async register(createUserDto: LoginUserDto) {
@@ -23,19 +25,28 @@ export class AuthService {
   }
 
   async login(loginUserDto: LoginUserDto) {
-    const { email, password } = loginUserDto;
+    try {
+      const { email, password } = loginUserDto;
 
-    const user = await User.findOne({ where: { email } });
-    if (user && (await bcrypt.compare(password, user.password))) {
-      const payload = { email: user.email, sub: user.id };
-      return {
-        user: {
-          id: user.id,
-          email: user.email,
-        },
-        access_token: this.jwtService.sign(payload),
-      };
+      const user = await User.findOne({ where: { email } });
+
+      if (!user) throw new Error('User not found');
+
+      if (user && (await bcrypt.compare(password, user.password))) {
+        const payload = { email: user.email, sub: user.id };
+        return {
+          user: {
+            id: user.id,
+            email: user.email,
+          },
+          access_token: this.jwtService.sign(payload),
+        };
+      } else {
+        throw new Error('Wrong password');
+      }
+    } catch (error) {
+      this.logger.error(`Unable to login: ${error}`);
+      throw new Error(`Unable to login: ${error}`);
     }
-    return null;
   }
 }
